16d9e18d07d064807fdb46c78af3c5be2657757b
horville <ewen.horville@etu.u-pec.fr>
2023-02-11 05:25:31 +0100

    Major Update, see workshop changelog for more info.

--- lua/autorun/client/modbridge_v2_spawnmenu.lua
+++ lua/autorun/client/modbridge_v2_spawnmenu.lua
@@ -1,112 +1,107 @@
  local size_1 = {
-    [1] = "models/modbridge_v2/corridors/size1/straight1.mdl",
-    [2] = "models/modbridge_v2/corridors/size1/straight2.mdl",
-    [3] = "models/modbridge_v2/corridors/size1/straight3.mdl",
-    [4] = "models/modbridge_v2/corridors/size1/turn.mdl",
-    [5] = "models/modbridge_v2/corridors/size1/3way.mdl",
-    [6] = "models/modbridge_v2/corridors/size1/4way.mdl",
-    [7] = "models/modbridge_v2/corridors/size1/deadend.mdl",
-    [8] = "models/modbridge_v2/corridors/size1/glassend.mdl",
-    [9] = "models/modbridge_v2/corridors/size1/glass_2p.mdl",
-    [10] = "models/modbridge_v2/corridors/size1/glass_1p.mdl",
-    [11] = "models/modbridge_v2/corridors/size1/glass_0p1.mdl",
-    [12] = "models/modbridge_v2/corridors/size1/glass_0p2.mdl",
-    [13] = "models/modbridge_v2/corridors/size1/glass_0p3.mdl",
-    [14] = "models/modbridge_v2/corridors/size1/straight1r.mdl",
-    [15] = "models/modbridge_v2/corridors/size1/straight2r.mdl",
-    [16] = "models/modbridge_v2/corridors/size1/straight3r.mdl",
-    [17] = "models/modbridge_v2/corridors/size1/straight1_sw.mdl",
-    [18] = "models/modbridge_v2/corridors/size1/straight2_sw.mdl",
-    [19] = "models/modbridge_v2/corridors/size1/straight3_sw.mdl",
+    "models/modbridge_v2/corridors/size1/straight1.mdl",
+    "models/modbridge_v2/corridors/size1/straight2.mdl",
+    "models/modbridge_v2/corridors/size1/straight3.mdl",
+    "models/modbridge_v2/corridors/size1/turn.mdl",
+    "models/modbridge_v2/corridors/size1/3way.mdl",
+    "models/modbridge_v2/corridors/size1/4way.mdl",
+    "models/modbridge_v2/corridors/size1/deadend.mdl",
+    "models/modbridge_v2/corridors/size1/glassend.mdl",
+    "models/modbridge_v2/corridors/size1/glass_2p.mdl",
+    "models/modbridge_v2/corridors/size1/glass_1p.mdl",
+    "models/modbridge_v2/corridors/size1/glass_0p1.mdl",
+    "models/modbridge_v2/corridors/size1/glass_0p2.mdl",
+    "models/modbridge_v2/corridors/size1/glass_0p3.mdl",
+    "models/modbridge_v2/corridors/size1/straight1_sw.mdl",
+    "models/modbridge_v2/corridors/size1/straight2_sw.mdl",
+    "models/modbridge_v2/corridors/size1/straight3_sw.mdl",
 }
 
 local size_2 = {
-    [1] = "models/modbridge_v2/corridors/size2/straight1.mdl",
-    [2] = "models/modbridge_v2/corridors/size2/straight2.mdl",
-    [3] = "models/modbridge_v2/corridors/size2/straight3.mdl",
-    [4] = "models/modbridge_v2/corridors/size2/turn.mdl",
-    [5] = "models/modbridge_v2/corridors/size2/3way.mdl",
-    [6] = "models/modbridge_v2/corridors/size2/4way.mdl",
-    [7] = "models/modbridge_v2/corridors/size2/deadend.mdl",
-    [8] = "models/modbridge_v2/corridors/size2/glassend.mdl",
-    [9] = "models/modbridge_v2/corridors/size2/glass_2p.mdl",
-    [10] = "models/modbridge_v2/corridors/size2/glass_1p.mdl",
-    [11] = "models/modbridge_v2/corridors/size2/glass_0p1.mdl",
-    [12] = "models/modbridge_v2/corridors/size2/glass_0p2.mdl",
-    [13] = "models/modbridge_v2/corridors/size2/glass_0p3.mdl",
-    [14] = "models/modbridge_v2/corridors/size2/straight1r.mdl",
-    [15] = "models/modbridge_v2/corridors/size2/straight2r.mdl",
-    [16] = "models/modbridge_v2/corridors/size2/straight3r.mdl",
-    [17] = "models/modbridge_v2/corridors/size2/straight1_sw.mdl",
-    [18] = "models/modbridge_v2/corridors/size2/straight2_sw.mdl",
-    [19] = "models/modbridge_v2/corridors/size2/straight3_sw.mdl",
+    "models/modbridge_v2/corridors/size2/straight1.mdl",
+    "models/modbridge_v2/corridors/size2/straight2.mdl",
+    "models/modbridge_v2/corridors/size2/straight3.mdl",
+    "models/modbridge_v2/corridors/size2/turn.mdl",
+    "models/modbridge_v2/corridors/size2/3way.mdl",
+    "models/modbridge_v2/corridors/size2/4way.mdl",
+    "models/modbridge_v2/corridors/size2/deadend.mdl",
+    "models/modbridge_v2/corridors/size2/glassend.mdl",
+    "models/modbridge_v2/corridors/size2/glass_2p.mdl",
+    "models/modbridge_v2/corridors/size2/glass_1p.mdl",
+    "models/modbridge_v2/corridors/size2/glass_0p1.mdl",
+    "models/modbridge_v2/corridors/size2/glass_0p2.mdl",
+    "models/modbridge_v2/corridors/size2/glass_0p3.mdl",
+    "models/modbridge_v2/corridors/size2/straight1_sw.mdl",
+    "models/modbridge_v2/corridors/size2/straight2_sw.mdl",
+    "models/modbridge_v2/corridors/size2/straight3_sw.mdl",
 }	
 
 local size_3 = {
-    [1] = "models/modbridge_v2/corridors/size3/straight1.mdl",
-    [2] = "models/modbridge_v2/corridors/size3/straight2.mdl",
-    [3] = "models/modbridge_v2/corridors/size3/straight3.mdl",
-    [4] = "models/modbridge_v2/corridors/size3/turn.mdl",
-    [5] = "models/modbridge_v2/corridors/size3/3way.mdl",
-    [6] = "models/modbridge_v2/corridors/size3/4way.mdl",
-    [7] = "models/modbridge_v2/corridors/size3/deadend.mdl",
-    [8] = "models/modbridge_v2/corridors/size3/glassend.mdl",
-    [9] = "models/modbridge_v2/corridors/size3/glass_2p.mdl",
-    [10] = "models/modbridge_v2/corridors/size3/glass_1p.mdl",
-    [11] = "models/modbridge_v2/corridors/size3/glass_0p1.mdl",
-    [12] = "models/modbridge_v2/corridors/size3/glass_0p2.mdl",
-    [13] = "models/modbridge_v2/corridors/size3/glass_0p3.mdl",
-    [14] = "models/modbridge_v2/corridors/size3/straight1r.mdl",
-    [15] = "models/modbridge_v2/corridors/size3/straight2r.mdl",
-    [16] = "models/modbridge_v2/corridors/size3/straight3r.mdl",
-    [17] = "models/modbridge_v2/corridors/size3/straight1_sw.mdl",
-    [18] = "models/modbridge_v2/corridors/size3/straight2_sw.mdl",
-    [19] = "models/modbridge_v2/corridors/size3/straight3_sw.mdl",
+    "models/modbridge_v2/corridors/size3/straight1.mdl",
+    "models/modbridge_v2/corridors/size3/straight2.mdl",
+    "models/modbridge_v2/corridors/size3/straight3.mdl",
+    "models/modbridge_v2/corridors/size3/turn.mdl",
+    "models/modbridge_v2/corridors/size3/3way.mdl",
+    "models/modbridge_v2/corridors/size3/4way.mdl",
+    "models/modbridge_v2/corridors/size3/deadend.mdl",
+    "models/modbridge_v2/corridors/size3/glassend.mdl",
+    "models/modbridge_v2/corridors/size3/glass_2p.mdl",
+    "models/modbridge_v2/corridors/size3/glass_1p.mdl",
+    "models/modbridge_v2/corridors/size3/glass_0p1.mdl",
+    "models/modbridge_v2/corridors/size3/glass_0p2.mdl",
+    "models/modbridge_v2/corridors/size3/glass_0p3.mdl",
+    "models/modbridge_v2/corridors/size3/straight1_sw.mdl",
+    "models/modbridge_v2/corridors/size3/straight2_sw.mdl",
+    "models/modbridge_v2/corridors/size3/straight3_sw.mdl",
 }
 
 local size_3x2 = {
-    [1] = "models/modbridge_v2/corridors/size3x2/straight1.mdl",
-    [2] = "models/modbridge_v2/corridors/size3x2/straight2.mdl",
-    [3] = "models/modbridge_v2/corridors/size3x2/straight3.mdl",
-    [4] = "models/modbridge_v2/corridors/size3x2/turn.mdl",
-    [5] = "models/modbridge_v2/corridors/size3x2/3way.mdl",
-    [6] = "models/modbridge_v2/corridors/size3x2/4way.mdl",
-    [7] = "models/modbridge_v2/corridors/size3x2/deadend.mdl",
-    [8] = "models/modbridge_v2/corridors/size3x2/glassend.mdl",
+    "models/modbridge_v2/corridors/size3x2/straight1.mdl",
+    "models/modbridge_v2/corridors/size3x2/straight2.mdl",
+    "models/modbridge_v2/corridors/size3x2/straight3.mdl",
+    "models/modbridge_v2/corridors/size3x2/turn.mdl",
+    "models/modbridge_v2/corridors/size3x2/3way.mdl",
+    "models/modbridge_v2/corridors/size3x2/4way.mdl",
+    "models/modbridge_v2/corridors/size3x2/deadend.mdl",
+    "models/modbridge_v2/corridors/size3x2/glassend.mdl",
 }
 
 local bridges = {
-    [1] = "models/modbridge_v2/corridors/bridges/triangle_s.mdl",
-    [2] = "models/modbridge_v2/corridors/bridges/triangle_sl.mdl",
-    [3] = "models/modbridge_v2/corridors/bridges/round_s.mdl",
+    "models/modbridge_v2/corridors/bridges/triangle_s.mdl",
+    "models/modbridge_v2/corridors/bridges/triangle_sl.mdl",
+    "models/modbridge_v2/corridors/bridges/round_s.mdl",
+    "models/modbridge_v2/corridors/bridges/triangle_sq.mdl",
 }		
 
 local splitters = {
-    [1] = "models/modbridge_v2/corridors/splitters/c3_1.mdl",
-    [2] = "models/modbridge_v2/corridors/splitters/c2_1.mdl",
-    [3] = "models/modbridge_v2/corridors/splitters/s1_2.mdl",
-    [4] = "models/modbridge_v2/corridors/splitters/s1_3.mdl",
-    [5] = "models/modbridge_v2/corridors/splitters/s2_3.mdl",
-    [6] = "models/modbridge_v2/corridors/splitters/f1_3.mdl",
-    [7] = "models/modbridge_v2/corridors/splitters/c3_1l.mdl",
-    [8] = "models/modbridge_v2/corridors/splitters/c3_1r.mdl",
-    [9] = "models/modbridge_v2/corridors/splitters/c3_1c.mdl",
-    [10] = "models/modbridge_v2/corridors/splitters/s3_32.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_1.mdl",
+    "models/modbridge_v2/corridors/splitters/c2_1.mdl",
+    "models/modbridge_v2/corridors/splitters/c2_3.mdl",
+    "models/modbridge_v2/corridors/splitters/s1_2.mdl",
+    "models/modbridge_v2/corridors/splitters/s1_3.mdl",
+    "models/modbridge_v2/corridors/splitters/s2_3.mdl",
+    "models/modbridge_v2/corridors/splitters/f1_3.mdl",
+    "models/modbridge_v2/corridors/splitters/s3_32.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_1l.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_1r.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_1c.mdl",
+    "models/modbridge_v2/corridors/splitters/c1_32.mdl",
+    "models/modbridge_v2/corridors/splitters/c2_32.mdl",
+    "models/modbridge_v2/corridors/splitters/c3_32.mdl",
 }
 
 local height_transfers = {
-    [1] = "models/modbridge_v2/corridors/height_transfers/s1.mdl",
-    [2] = "models/modbridge_v2/corridors/height_transfers/s2.mdl",
-    [3] = "models/modbridge_v2/corridors/height_transfers/s3.mdl",
-    [4] = "models/modbridge_v2/corridors/height_transfers/sp3_1.mdl",
-    [5] = "models/modbridge_v2/corridors/height_transfers/sp3_2.mdl",
+    "models/modbridge_v2/corridors/height_transfers/s1.mdl",
+    "models/modbridge_v2/corridors/height_transfers/s2.mdl",
+    "models/modbridge_v2/corridors/height_transfers/s3.mdl",
+    "models/modbridge_v2/corridors/height_transfers/sp3_1.mdl",
+    "models/modbridge_v2/corridors/height_transfers/sp3_2.mdl",
 }
 
-local external_decors = {
-    [1] = "models/modbridge_v2/external_decor/round_wedge1.mdl",
-    [2] = "models/modbridge_v2/external_decor/round_wedge2.mdl",
-    [3] = "models/modbridge_v2/external_decor/round_wedge3.mdl",
-    [4] = "models/modbridge_v2/external_decor/round_wedgec.mdl",
+local miscellaneous = {
+    "models/modbridge_v2/corridors/misc/pillar.mdl",
+    "models/modbridge_v2/corridors/misc/pillar_double.mdl",
+    "models/modbridge_v2/corridors/misc/wall.mdl",
 }
 
 hook.Add( "PopulatePropMenu", "Modbridge V2", function()
@@ -199,13 +194,13 @@ hook.Add( "PopulatePropMenu", "Modbridge V2", function()
 
     table.insert( contents, {
         type = "header",
-        text = "External Decors"
+        text = "Miscellaneous"
     })
 
-    for i = 1,#external_decors do
+    for i = 1,#miscellaneous do
         table.insert( contents, {
             type = "model",
-            model = external_decors[i]
+            model = miscellaneous[i]
         })
     end
 
